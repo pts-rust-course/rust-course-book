c2d9caed4022e640d6167e6f5aecdc55eff238d1
Vladimir Latypov <donrumata03@gmail.com>
2023-09-16 18:07:16 +0300

    Fix build script, modify book

--- build.cmd
+++ build.cmd
@@ -1,3 +1,3 @@
 :: Should be run when the working tree is clean
-git switch view && git reset --hard main && rmdir /s /q ".\docs" && mdbook build && git add . && git commit -m "Build mdBook" && git push origin view && git switch main
+git switch view && git reset --hard main && (rmdir /s /q ".\docs" || break) && mdbook build && git add -f build && git commit -m "Build mdBook" && git push origin view && git switch main
 :: Would be a huge leg shot if there are uncommitted changes without «&&»
