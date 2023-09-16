dd1ba13fa302845ae7b7321f24c8eed984beab57
Vladimir Latypov <donrumata03@gmail.com>
2023-09-16 18:09:38 +0300

    Switch to main anyway

--- build.cmd
+++ build.cmd
@@ -1,3 +1,5 @@
 :: Should be run when the working tree is clean
-git switch view && git reset --hard main && (rmdir /s /q ".\docs" || break) && mdbook build && git add -f build && git commit -m "Build mdBook" && git push origin view && git switch main
+git switch view && git reset --hard main && (rmdir /s /q ".\docs" || break) && mdbook build && git add -f docs && git commit -m "Build mdBook" && git push -f origin view
 :: Would be a huge leg shot if there are uncommitted changes without «&&»
+
+git switch main
