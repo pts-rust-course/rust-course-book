535a774cccdf3e7f623201ab60f56f417889d82d
Vladimir Latypov <donrumata03@gmail.com>
2023-09-16 18:17:30 +0300

    Add script status

--- build.cmd
+++ build.cmd
@@ -1,5 +1,5 @@
 :: Should be run when the working tree is clean
-git switch view && git reset --hard main && (rmdir /s /q ".\docs" || break) && mdbook build && git add -f docs && git commit -m "Build mdBook" && git push -f origin view
+git switch view && git reset --hard main && (rmdir /s /q ".\docs" || break) && mdbook build && git add -f docs && git commit -m "Build mdBook" && git push -f origin view && echo Book update done!
 :: Would be a huge leg shot if there are uncommitted changes without «&&»
 
 git switch main
